2b9e116865291d7f3c851f820fb1298668d29b56
Jenn Tustin <jenn.tustin@gmail.com>
2017-01-29 17:23:55 -0500

    Remove notes

--- lib/currency.rb
+++ lib/currency.rb
@@ -40,9 +40,6 @@ class Currency
   def *(factor)
     if factor.is_a? Float or factor.is_a? Integer
       Currency.new(@code, @amount * factor)
-    #else
-      #factor = Currency.new()
-      #need to deal with curr object given with symbol and amount
     end
   end
 
@@ -50,5 +47,3 @@ class Currency
     "#{@code} #{@amount}"
   end
 end
-
-# Currency() must be able to take one argument with a currency symbol embedded in it, like "$1.20" or "€ 7.00", and figure out the correct currency code. It can also take two arguments, one being the amount and the other being the currency code.
